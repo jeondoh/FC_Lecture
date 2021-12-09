083e841b27c122e437739ceb33d064de0f78df7f
jundoh <jundo1302@gmail.com>
2021-12-09 21:07:06 +0900

    feat: Make drop down menu, side bar
    Use lodash, gsap

--- Part01/starbucks/js/main.js
+++ Part01/starbucks/js/main.js
@@ -15,3 +15,25 @@ searchInputEl.addEventListener('blur', function(){
   searchInputEl.setAttribute('placeholder', '');
 });
 
+const badgeEl = document.querySelector('header .badges');
+
+// lodash 사용
+window.addEventListener('scroll', _.throttle(function() {
+  // 현재 스크롤 위치(윈도우 기준)
+  if(window.scrollY > 500){
+    // 배치 숨기기 (gasp 사용)
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display : 'none'
+    });
+  } else {
+    // 배치 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display : 'block'
+    });
+  }
+}, 300));
+
+//_.throttle(함수, 시간)
