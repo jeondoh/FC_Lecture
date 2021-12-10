16d8c8f215af324231071211bf156bcb19aaf7c9
jundoh <jundo1302@gmail.com>
2021-12-10 21:42:27 +0900

    feat: make notice, slide, promotion, rewards
    TODO: add youtube iframe api

--- Part01/starbucks/js/main.js
+++ Part01/starbucks/js/main.js
@@ -35,5 +35,57 @@ window.addEventListener('scroll', _.throttle(function() {
     });
   }
 }, 300));
-
 //_.throttle(함수, 시간)
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index){
+  // gsap.to(요소, 지속시간, 옵션);
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
+});
+
+
+// 공지사항 Swiper
+new Swiper('.notice-line .swiper', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+
+// 프로모션 슬라이드
+new Swiper(".promotion .swiper", {
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이게
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true, // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+
+// 프로모션 보이기&숨기기
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function(){
+  isHidePromotion = !isHidePromotion;
+  if(isHidePromotion){
+    // 숨김 처리
+    promotionEl.classList.add('hide');
+  }else{
+    // 보이기 처리
+    promotionEl.classList.remove('hide');
+  }
+});
