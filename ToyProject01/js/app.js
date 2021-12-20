e9529bd13ba1d138a5a5ebcd205e68f2a3f21a6e
jundoh <jundo1302@gmail.com>
2021-12-21 02:18:37 +0900

    feat: ToyProject home view done.
    TODO: get json data, other pages

---
+++ ToyProject01/js/app.js
@@ -0,0 +1,22 @@
+
+/*
+const getData = async () => {
+    // let res = await fetch( "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f6e4d3d3-c52c-4ea8-b665-968a3b17c5ea/bank.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20211220%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211220T043619Z&X-Amz-Expires=86400&X-Amz-Signature=60ceecfa1549eaf893c717343a6e87da3c01cc8990f14d385cb53352d6b51be5&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22bank.json%22&x-id=GetObject" );
+    let res = await fetch( "https://doa0223.synology.me:8000/getJson" );
+    let data = await res.json();
+    console.log(data);
+    return data
+}
+*/
+// document.addEventListener("DOMContentLoaded", () => { getData() });
+
+/*
+fetch('https://jeondoh.github.io/FC_Lecture/ToyProject01/files/A1jo.json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+    });
+
+*/
