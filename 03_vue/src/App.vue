928620a3b362ee697ca19f08ad96a6ba46eda466
jundoh <jundo1302@gmail.com>
2022-01-03 01:09:53 +0900

    feat: vue parctice
    if, for, component, handling

---
+++ 03_vue/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1 @click="increase">{{ count }}</h1>
+  <div v-if="count > 4">
+    4보다 큽니다!
+  </div>
+  <ul>
+    <Fruit
+        v-for="fruit in fruits" :key="fruit" :name="fruit">
+      {{ fruit }}
+    </Fruit>
+  </ul>
+</template>
+
+<script>
+import Fruit from "~/components/Fruit";
+export default {
+  components:{
+    // 이름과 컴포넌트가 같을 경우 생략 가능
+    // Fruit: Fruit
+    Fruit
+  },
+  data(){
+    return{
+      count: 0,
+      fruits: ['Apple', 'Banana', "Cherry"]
+    }
+  },
+  methods: {
+    increase() {
+      this.count++;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  h1{
+    font-size: 50px;
+    color: royalblue;
+  }
+  ul{
+    li{
+      font-size: 40px;
+    }
+  }
+</style>
---
+++ 03_vue/src/components/Fruit.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>{{name}}?!</li>
+</template>
+
+<script>
+export default{
+  props:{
+    name:{
+      type: String,
+      default:''
+    }
+  }
+}
+</script>
+<!-- scoped: 현재 컴포넌트만 지정(유효범위) -->
+<style scoped lang="scss">
+  h1{
+    color:red !important;
+  }
+</style>
