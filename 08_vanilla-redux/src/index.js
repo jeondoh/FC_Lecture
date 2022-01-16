e52e7a525432fceed047e60b5f7df6bf3cfe87a5
jundoh <jundo1302@gmail.com>
2022-01-16 19:15:57 +0900

    feat: make todos with redux (practice)

---
+++ 08_vanilla-redux/src/index.js
@@ -0,0 +1,69 @@
+import {createStore} from "redux";
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const addTodo = text => {
+  return {
+    type: ADD_TODO,
+    text,
+  }
+}
+
+const deleteTodo = id => {
+  return {
+    type: DELETE_TODO,
+    id,
+  }
+}
+
+const reducer = (state = [], action) => {
+  switch (action.type){
+    case ADD_TODO:
+      return [{text:action.text, id:Date.now()}, ...state];
+    case DELETE_TODO:
+      return state.filter(todo => todo.id !== parseInt(action.id));
+    default:
+      return state;
+  }
+}
+const store = createStore(reducer);
+store.subscribe(() => console.log(store.getState()))
+
+const dispatchAddTodo = (text) => {
+  store.dispatch(addTodo(text))
+}
+
+const dispatchDeleteTodo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteTodo(id))
+}
+
+const paintTodos = () => {
+  const todos = store.getState();
+  ul.innerHTML = "";
+  todos.forEach(todo => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.innerText = "delete";
+    btn.addEventListener("click", dispatchDeleteTodo);
+    li.id = todo.id;
+    li.innerText = todo.text;
+    ul.appendChild(li);
+    li.appendChild(btn);
+  })
+}
+store.subscribe(paintTodos)
+
+const onSubmit = e => {
+  e.preventDefault();
+  const todo = input.value;
+  input.value = "";
+  dispatchAddTodo(todo);
+}
+
+form.addEventListener('submit', onSubmit);
